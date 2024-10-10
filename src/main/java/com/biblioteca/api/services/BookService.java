b670e59ff158d1ab600655ab902af7bec9b88169
Iran <iranildosousa1234@gmail.com>
2024-10-10 18:49:13 -0300

    Add BookRepository & Dependencies

--- src/main/java/com/biblioteca/api/services/BookService.java
+++ src/main/java/com/biblioteca/api/services/BookService.java
@@ -1,9 +1,14 @@
 package com.biblioteca.api.services;
 
+import com.biblioteca.api.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
+
+    @Autowired
+    BookRepository bookRepository;
 
     public String helloWorld(String name){
         return "Iniciando projeto de listas de livros, olá " + name;
